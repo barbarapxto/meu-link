c1cd56d1bebf29fc51e08c40882120e2eb158b33
barbarapxto <barbarapeixoto712@gmail.com>
2022-02-03 08:00:10 -0300

    Estilização da página Links

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,7 +1,37 @@
+import './links.css';
+import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
 function Links(){
     return(
-        <div>
-            <h1>Página de Links</h1>
+        <div className="links-container">
+            <div className="links-header">
+                <Link to="/">
+                    <FiArrowLeft size={38} color="#fff" />
+                </Link>
+                <h1>Meus Links</h1>
+            </div>
+
+            <div className="links-item">
+                <button className="link">
+                    <FiLink size={18} color="#fff" />
+                    https://sujeitoprogramador.com
+                </button>
+                <button className="link-delete">
+                    <FiTrash size={24} color="#FF5454"/>
+                </button>
+            </div>
+
+            <div className="links-item">
+                <button className="link">
+                    <FiLink size={18} color="#fff" />
+                    https://sujeitoprogramador.com
+                </button>
+                <button className="link-delete">
+                    <FiTrash size={24} color="#FF5454"/>
+                </button>
+            </div>
+
         </div>
     )
 }
